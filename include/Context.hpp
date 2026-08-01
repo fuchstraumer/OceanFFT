723b776ca71ec1e2c13ed027d8c956a45497323e
Fuchs <fuchstraumer@gmail.com>
2026-08-01 09:54:48 -0700

    Get "Context" object finished up
    Expand parameters struct significantly, use std::expected<> for most of the creation functions, add function to init GLFW window. Will need to return for a few things - note several todo comments

--- include/Context.hpp
+++ include/Context.hpp
@@ -1,79 +1,106 @@
 #pragma once
-#ifndef WEB_GPU_CONTEXT_HPP
-#define WEB_GPU_CONTEXT_HPP
+
+#ifndef VELOX_WEB_GPU_CONTEXT_HPP
+#define VELOX_WEB_GPU_CONTEXT_HPP
+#include "VeloxErrors.hpp"
 #include <webgpu/webgpu_cpp.h>
 #include <cstdint>
 #include <string_view>
+#include <vector>
 
 #ifndef __EMSCRIPTEN__
 struct GLFWwindow;
 #endif
 
-struct ContextCreateInfo
+namespace velox
 {
-    std::string_view ApplicationName{ "WebGPU App" };
-    std::vector<wgpu::FeatureName> RequiredFeatures;
-    // Undefined => pick the first format the surface reports as supported
-    wgpu::TextureFormat PreferredSurfaceFormat{ wgpu::TextureFormat::Undefined };
-#ifdef __EMSCRIPTEN__
-    std::string_view CanvasSelector{ "#canvas" };
+    
+    struct ContextCreateInfo
+    {
+        uint32_t InitialWidth{ 800u };
+        uint32_t InitialHeight{ 600u };
+        std::string_view ApplicationName{ "WebGPU App" };
+        // This is for *device* features only
+        std::vector<wgpu::FeatureName> RequiredFeatures;
+        wgpu::FeatureLevel FeatureLevel{ wgpu::FeatureLevel::Core };
+        wgpu::PowerPreference PowerPreference{ wgpu::PowerPreference::HighPerformance };
+
+        // following are swapchain parameters: named "preferred" because we will try to use them,
+        // but won't crash or fail if the surface doesn't support them (support varies a LOT ime)
+        // Undefined => pick the first format the surface reports as supported
+        wgpu::TextureFormat PreferredSurfaceFormat{ wgpu::TextureFormat::Undefined };
+        // todo: how does HDR support actually work? we'll need a tonemapper too....
+        wgpu::PredefinedColorSpace PreferredColorSpace{ wgpu::PredefinedColorSpace::SRGB };
+        wgpu::PresentMode PreferredPresentationMode{ wgpu::PresentMode::Fifo };
+    #ifdef __EMSCRIPTEN__
+        std::string_view CanvasSelector{ "#canvas" };
+    #endif
+    };
+
+    /**
+     * @brief Owns the classical Instance/Adapter/Device trio, but also has full
+     * ownership of the surface and queue. Also handles a few bookkeeping and
+     * common callbacks like device losses, surface reconfig, etc. This class
+     * mostly exists to centralize setup. 
+     * 
+     * Based on my RhiSystem implementation from DiamondDogs repo. Does not 
+     * allow access to queues, resources, or command buffers: purely holds
+     * the baseline objects we need to get a WebGPU context online.
+    */
+    class Context
+    {
+        Context(const Context&) = delete;
+        Context& operator=(const Context&) = delete;
+    public:
+
+    #ifdef __EMSCRIPTEN__
+        explicit Context(const ContextCreateInfo& createInfo);
+    #else
+        Context(const ContextCreateInfo& createInfo, GLFWwindow* nativeWindow);
+    #endif
+        ~Context();
+
+        void Resize(uint32_t width, uint32_t height);
+        wgpu::TextureView AcquireNextFrame();
+        void Present();
+
+        const wgpu::Instance& GetInstance() const noexcept;
+        const wgpu::Adapter& GetAdapter() const noexcept;
+        const wgpu::Device& GetDevice() const noexcept;
+        const wgpu::Queue& GetQueue() const noexcept;
+        const wgpu::Surface& GetSurface() const noexcept;
+        wgpu::TextureFormat GetSurfaceFormat() const noexcept;
+
+        bool HasFeature(wgpu::FeatureName feature) const noexcept;
+
+    private:
+
+        std::expected<wgpu::Instance, RhiError> requestInstance(const ContextCreateInfo& createInfo);
+        std::expected<wgpu::Adapter, RhiError> requestAdapter(const ContextCreateInfo& createInfo);
+        std::expected<wgpu::Device, RhiError> requestDevice(const ContextCreateInfo& createInfo);
+    #ifdef __EMSCRIPTEN__
+        std::expected<wgpu::Surface, RhiError> createSurface(const ContextCreateInfo& createInfo);
+    #else
+        // need to make sure a valid GLFWwindow* is created before we create the surface
+        std::expected<GLFWwindow*, RhiError> createNativeWindow(const ContextCreateInfo& createInfo);
+        std::expected<wgpu::Surface, RhiError> createSurface(const ContextCreateInfo& createInfo, GLFWwindow* nativeWindow);
+    #endif
+
+        void configureSurface(const ContextCreateInfo& createInfo);
+
+        wgpu::Instance instance;
+        wgpu::Adapter adapter;
+        wgpu::Device device;
+        wgpu::Queue queue;
+#ifndef __EMSCRIPTEN__
+        GLFWwindow* nativeWindow{ nullptr };
 #endif
-};
+        wgpu::Surface surface;
+        wgpu::TextureFormat surfaceFormat{ wgpu::TextureFormat::Undefined };
+        // we store the surface config to make reconfiguring not need the whole create info
+        wgpu::SurfaceConfiguration surfaceConfig{};
+    };
 
-/**
- * @brief Owns the classical Instance/Adapter/Device trio, but also has full
- * ownership of the surface and queue. Also handles a few bookkeeping and
- * common callbacks like device losses, surface reconfig, etc. This class
- * mostly exists to centralize setup. 
- * 
- * Based on my RhiSystem implementation from DiamondDogs repo
-*/
-class Context
-{
-    Context(const Context&) = delete;
-    Context& operator=(const Context&) = delete;
-public:
+} // namespace velox
 
-#ifdef __EMSCRIPTEN__
-    explicit Context(const ContextCreateInfo& createInfo);
-#else
-    Context(const ContextCreateInfo& createInfo, GLFWwindow* nativeWindow);
-#endif
-    ~Context();
-
-    void Resize(uint32_t width, uint32_t height);
-    wgpu::TextureView AcquireNextFrame();
-    void Present();
-
-    const wgpu::Instance& GetInstance() const noexcept;
-    const wgpu::Adapter& GetAdapter() const noexcept;
-    const wgpu::Device& GetDevice() const noexcept;
-    const wgpu::Queue& GetQueue() const noexcept;
-    const wgpu::Surface& GetSurface() const noexcept;
-    wgpu::TextureFormat GetSurfaceFormat() const noexcept;
-
-    bool HasFeature(wgpu::FeatureName feature) const noexcept;
-
-private:
-
-    void requestAdapter(const ContextCreateInfo& createInfo);
-    void requestDevice(const ContextCreateInfo& createInfo);
-#ifdef __EMSCRIPTEN__
-    void createSurface(const ContextCreateInfo& createInfo);
-#else
-    void createSurface(const ContextCreateInfo& createInfo, GLFWwindow* nativeWindow);
-#endif
-
-    void configureSurface(uint32_t width, uint32_t height);
-
-    wgpu::Instance instance;
-    wgpu::Adapter adapter;
-    wgpu::Device device;
-    wgpu::Queue queue;
-    wgpu::Surface surface;
-    wgpu::TextureFormat surfaceFormat{ wgpu::TextureFormat::Undefined };
-    uint32_t surfaceWidth{ 0u };
-    uint32_t surfaceHeight{ 0u };
-};
-
-#endif //!WEB_GPU_CONTEXT_HPP
+#endif // !VELOX_WEB_GPU_CONTEXT_HPP
--- src/Context.cpp
+++ src/Context.cpp
@@ -1,26 +1,56 @@
 #include "Context.hpp"
 #include <format>
 #include <iostream>
+#include <print>
 #ifndef __EMSCRIPTEN__
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu_glfw.h>
 #endif
+#include <unordered_map>
+#include <algorithm>
 
 namespace
-{   
+{
+    static const std::unordered_map<wgpu::ErrorType, std::string_view> ErrorTypeStrings
+    {
+        { wgpu::ErrorType::NoError, "No Error" },
+        { wgpu::ErrorType::Validation, "Validation" },
+        { wgpu::ErrorType::OutOfMemory, "Out of Memory" },
+        { wgpu::ErrorType::Internal, "Internal" },
+        { wgpu::ErrorType::Unknown, "Unknown" }
+    };
+
+    static const std::unordered_map<wgpu::DeviceLostReason, std::string_view> DeviceLostReasonStrings
+    {
+        { wgpu::DeviceLostReason::Unknown, "Unknown" },
+        { wgpu::DeviceLostReason::Destroyed, "Destroyed" },
+        { wgpu::DeviceLostReason::CallbackCancelled, "Callback Cancelled" },
+        { wgpu::DeviceLostReason::FailedCreation, "Creation Failed" }
+    };
+
     // todo: we should sink these somewhere more portable, and which could actually give us debug info in live clients maybe?
     void LogUncapturedError(const wgpu::Device&, wgpu::ErrorType type, wgpu::StringView message)
     {
-        std::cerr << std::format("[wgpu] Uncaptured error ({}): {}\n",
-            static_cast<int>(type), std::string_view(message.data, message.length));
+        const auto iter = ErrorTypeStrings.find(type);
+        const std::string_view typeStr = (iter != ErrorTypeStrings.end()) ? iter->second : "Unknown";
+        std::println(stderr,
+                     "[wgpu] Uncaptured error | Type \"{}\" | Message: {}",
+                     typeStr, std::string_view(message.data, message.length));
     }
 
     void LogDeviceLost(const wgpu::Device&, wgpu::DeviceLostReason reason, wgpu::StringView message)
     {
-        std::cerr << std::format("[wgpu] Device lost ({}): {}\n",
-            static_cast<int>(reason), std::string_view(message.data, message.length));
+        // note that this is also called for routine destruction, so messages from here don't always mean something went wrong
+        const auto it = DeviceLostReasonStrings.find(reason);
+        const std::string_view reasonStr = (it != DeviceLostReasonStrings.end()) ? it->second : "Unknown";
+        std::println(stderr,
+                     "[wgpu] Device lost | Reason: \"{}\" | Message: {}",
+                     reasonStr, std::string_view(message.data, message.length));
     }
 }
+
+namespace velox
+{
 
 #ifdef __EMSCRIPTEN__
 Context::Context(const ContextCreateInfo& createInfo)
@@ -29,4 +59,251 @@ Context::Context(const ContextCreateInfo& createInfo, GLFWwindow* nativeWindow)
 #endif
 {
     wgpu::InstanceDescriptor instanceDesc{};
+#ifndef __EMSCRIPTEN__
+    const wgpu::InstanceFeatureName requiredFeatures[] = { wgpu::InstanceFeatureName::TimedWaitAny };
+    instanceDesc.requiredFeatureCount = std::size(requiredFeatures);
+    instanceDesc.requiredFeatures = requiredFeatures;
+    instance = ValidOrExit(requestInstance(createInfo));
+#endif
+
+    adapter = ValidOrExit(requestAdapter(createInfo));
+    device = ValidOrExit(requestDevice(createInfo));
+    queue = device.GetQueue();
+
+#ifdef __EMSCRIPTEN__
+    surface = ValidOrExit(createSurface(createInfo));
+#else
+    // createnativewindow() also initializes glfw, mostly just to keep code tidy
+    nativeWindow = ValidOrExit(createNativeWindow(createInfo));
+    surface = ValidOrExit(createSurface(createInfo, nativeWindow));
+#endif
 }
+
+Context::~Context()
+{
+    glfwDestroyWindow(nativeWindow);
+    glfwTerminate();
+}
+
+std::expected<wgpu::Instance, RhiError> Context::requestInstance(const ContextCreateInfo& createInfo)
+{
+    wgpu::InstanceDescriptor instanceDesc{};
+    instanceDesc.nextInChain = nullptr;
+    wgpu::Instance instance = wgpu::CreateInstance(&instanceDesc);
+    if (!instance)
+    {
+        return std::unexpected(RhiError::AdapterRequestFailed);
+    }
+    return instance;
+}
+
+std::expected<wgpu::Adapter, RhiError> Context::requestAdapter(const ContextCreateInfo& createInfo)
+{
+    wgpu::RequestAdapterOptions options{};
+#ifndef __EMSCRIPTEN__
+    options.backendType = wgpu::BackendType::Vulkan; // default to vulkan, since we know it best
+#endif
+    options.featureLevel = createInfo.FeatureLevel;
+    options.powerPreference = createInfo.PowerPreference;
+    // todo: compatible surface parameter. unused right now, but I think it could let us query for HDR backbuffer support?
+    // diamonddogs does this through glfw and win32 api calls, but I think we can do it through wgpu directly if we pass the surface here.
+
+    wgpu::Adapter result_adapter;
+    wgpu::Future future = instance.RequestAdapter(&options, wgpu::CallbackMode::WaitAnyOnly,
+        [&result_adapter](wgpu::RequestAdapterStatus status, wgpu::Adapter result, wgpu::StringView message)
+        {
+            if (status == wgpu::RequestAdapterStatus::Success)
+            {
+                result_adapter = std::move(result);
+            }
+            else
+            {
+                result_adapter = wgpu::Adapter{}; // ensure it's empty
+                std::println(stderr, "RequestAdapter failed: {}", std::string_view(message.data, message.length));
+            }
+        });
+
+    instance.WaitAny(future, UINT64_MAX);
+    
+    if (!result_adapter)
+    {
+        return std::unexpected(RhiError::AdapterRequestFailed);
+    }
+
+    return result_adapter;
+}
+
+std::expected<wgpu::Device, RhiError> Context::requestDevice(const ContextCreateInfo& createInfo)
+{
+    wgpu::DeviceDescriptor deviceDesc{};
+    deviceDesc.label = createInfo.ApplicationName;
+    deviceDesc.requiredFeatureCount = createInfo.RequiredFeatures.size();
+    deviceDesc.requiredFeatures = createInfo.RequiredFeatures.data();
+    deviceDesc.SetUncapturedErrorCallback(LogUncapturedError);
+    deviceDesc.SetDeviceLostCallback(wgpu::CallbackMode::AllowSpontaneous, LogDeviceLost);
+    // todo: leaving required limits blank clamps to minspec, which is fine for now. will have to assess this better in future
+    //deviceDesc.requiredLimits = nullptr;
+    if (!createInfo.RequiredFeatures.empty())
+    {
+        deviceDesc.requiredFeatureCount = createInfo.RequiredFeatures.size();
+        deviceDesc.requiredFeatures = createInfo.RequiredFeatures.data();
+    }
+    else
+    {
+        deviceDesc.requiredFeatureCount = 0;
+        deviceDesc.requiredFeatures = nullptr;
+    }
+
+    wgpu::Device result_device;
+
+    wgpu::Future future = adapter.RequestDevice(&deviceDesc, wgpu::CallbackMode::WaitAnyOnly,
+        [&result_device](wgpu::RequestDeviceStatus status, wgpu::Device result, wgpu::StringView message)
+        {
+            if (status == wgpu::RequestDeviceStatus::Success)
+            {
+                result_device = std::move(result);
+            }
+            else
+            {
+                result_device = wgpu::Device{}; // ensure it's empty
+                std::println(stderr, "RequestDevice failed: {}", std::string_view(message.data, message.length));
+            }
+        });
+
+    instance.WaitAny(future, UINT64_MAX);
+
+    if (!result_device)
+    {
+        return std::unexpected(RhiError::DeviceRequestFailed);
+    }
+
+    return result_device;
+}
+
+#ifdef __EMSCRIPTEN__
+std::expected<wgpu::Surface, RhiError> Context::createSurface(const ContextCreateInfo& createInfo)
+{
+    wgpu::EmscriptenSurfaceSourceCanvasHTMLSelector canvasDesc{};
+    canvasDesc.selector = createInfo.CanvasSelector.c_str();
+    wgpu::SurfaceColorManagement colorDesc{};
+    colorDesc.colorSpace = createInfo.PreferredColorSpace;
+    // todo: what is Extended tonemapping? do we not have the ability to run our own? is this mobile weirdness?
+    colorDesc.toneMappingMode = wgpu::ToneMappingMode::Standard;
+    // make sure chaining is set right
+    canvasDesc.nextInChain = &colorDesc;
+    colorDesc.nextInChain = nullptr;
+
+    wgpu::SurfaceDescriptor surfaceDesc{};
+    surfaceDesc.nextInChain = &canvasDesc;
+    surface = instance.CreateSurface(&surfaceDesc);
+    if (!surface)
+    {
+        return std::unexpected(RhiError::SurfaceConfigurationFailed);
+    }
+    return surface;
+}
+#else
+std::expected<GLFWwindow*, RhiError> Context::createNativeWindow(const ContextCreateInfo& createInfo)
+{
+    if (!glfwInit())
+    {
+        return std::unexpected(RhiError::GLFWInitFailed);
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // don't create an OpenGL context
+    GLFWwindow* window = glfwCreateWindow(createInfo.InitialWidth, createInfo.InitialHeight, createInfo.ApplicationName.data(), nullptr, nullptr);
+    if (!window)
+    {
+        return std::unexpected(RhiError::GLFWWindowCreationFailed);
+    }
+
+    return window;
+}
+
+
+std::expected<wgpu::Surface, RhiError> Context::createSurface(const ContextCreateInfo& /*createInfo*/, GLFWwindow* nativeWindow)
+{
+    // todo: this GLFW shim sets the descriptor based on GLFW hints, but for things like colorspaces this won't pass through
+    // at least it didn't in DiamondDogs, not without a good bit of extra work
+    surface = wgpu::glfw::CreateSurfaceForWindow(instance, nativeWindow);
+    if (!surface)
+    {
+        return std::unexpected(RhiError::SurfaceCreationFailed);
+    }
+    return surface;
+}
+#endif
+
+void Context::configureSurface(const ContextCreateInfo& createInfo)
+{
+    wgpu::SurfaceCapabilities capabilities{};
+    surface.GetCapabilities(adapter, &capabilities);
+
+    auto format_match =
+        [&createInfo](wgpu::TextureFormat format)
+        { 
+            return format == createInfo.PreferredSurfaceFormat;
+        };
+    // traverse array of caps and use std::find_if to find the first format
+    // that matches our preferred format. then we go on to guessing. heuristically. :)
+    auto format_iter = std::find_if(capabilities.formats,
+                                    capabilities.formats + capabilities.formatCount,
+                                    format_match);
+
+    surfaceConfig.device = device;
+    surfaceConfig.format = surfaceFormat;
+    surfaceConfig.usage = wgpu::TextureUsage::RenderAttachment;
+    surfaceConfig.width = createInfo.InitialWidth;
+    surfaceConfig.height = createInfo.InitialHeight;
+    // todo: assess later how/if we may want to change alpha mode
+    surfaceConfig.alphaMode = wgpu::CompositeAlphaMode::Auto;
+    surfaceConfig.presentMode = createInfo.PreferredPresentationMode;
+    surface.Configure(&surfaceConfig);
+
+}
+
+void Context::Resize(uint32_t width, uint32_t height)
+{
+    if (width == 0u || height == 0u)
+    {
+        return; // minimized / zero-size - skip until it's real again
+    }
+    // just update dims, and then reconfigure
+    surfaceConfig.width = width;
+    surfaceConfig.height = height;
+    surface.Configure(&surfaceConfig);
+}
+
+wgpu::TextureView Context::AcquireNextFrame()
+{
+    wgpu::SurfaceTexture surfaceTexture{};
+    surface.GetCurrentTexture(&surfaceTexture);
+
+    switch (surfaceTexture.status)
+    {
+    case wgpu::SurfaceGetCurrentTextureStatus::SuccessOptimal:
+        return surfaceTexture.texture.CreateView();
+    case wgpu::SurfaceGetCurrentTextureStatus::SuccessSuboptimal:
+        std::println(stderr, "[velox][wgpu] Next frame acquisition returned SuccessSuboptimal");
+        return surfaceTexture.texture.CreateView();
+    default:
+        // todo: this should be a std::expected return
+        return wgpu::TextureView{};
+    }
+}
+
+void Context::Present()
+{
+#ifndef __EMSCRIPTEN__
+    // Emscripten presents implicitly at the end of each browser frame;
+    // calling Present() there is a validation error.
+    surface.Present();
+#endif
+}
+
+bool Context::HasFeature(wgpu::FeatureName feature) const noexcept
+{
+    return device.HasFeature(feature);
+}
+
+} // namespace velox
