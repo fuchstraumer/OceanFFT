ba93b229b1b713400b917935a6c609e106175f85
Fuchs <fuchstraumer@gmail.com>
2026-08-04 15:35:29 -0700

    Move to CMakePresets.json for iteration speed
    Driven mad enough by how slow iteration can be with legacy kits - especially since modifying any of *my* source code made us recheck and rebuild some targets (especially LLVM!) I've attempted to mvoe this repo to CMakePresets.json. Should use Ninja consistently now, which is already behaving much better. Also had an easier time actually getting Clang to play along.
    
    Update CMakeLists and ConfigureDawn.cmake slightly, mostly to make things more portable, ensmallen build targets, and remove some things I shouldn't be adding to CMAKE_CXX_FLAGS

--- cmake/ConfigureDawn.cmake
+++ cmake/ConfigureDawn.cmake
@@ -5,9 +5,16 @@ set(VulkanHeaders_DIR $ENV{VULKAN_SDK}/Include)
 set(DAWN_ENABLE_DESKTOP_GL OFF)
 set(DAWN_ENABLE_OPENGLES OFF)
 set(DAWN_ENABLE_D3D11 OFF)
+# we *do* want to make sure we enable DXC, as it's required to enable and use
+# both subgroups and shader-f16
+set(DAWN_USE_BUILT_DXC ON)
+# Vulkan currently broken because of the Nvidia fp16 bugs, disabling
+set(DAWN_ENABLE_VULKAN OFF)
+set(DAWN_ENABLE_SPIRV_VALIDATION OFF)
 set(DAWN_BUILD_SAMPLES OFF)
+set(DAWN_BUILD_PROTOBUF OFF)
 # using GLFW
-set(DAWN_ENABLE_WINDOWS_UI OFF)
+set(DAWN_USE_WINDOWS_UI OFF)
 # not using fuzzers
 set(TINT_BUILD_FUZZERS OFF)
 set(TINT_BUILD_DOCS OFF)
@@ -18,6 +25,6 @@ set(TINT_BUILD_CMD_TOOLS OFF)
 set(TINT_BUILD_GLSL_VALIDATOR OFF)
 set(TINT_BUILD_SPV_WRITER OFF)
 set(TINT_BUILD_SPV_READER OFF)
-# we *do* want to make sure we enable DXC, as it's required to enable and use
-# both subgroups and shader-f16
-set(DAWN_USE_BUILT_DXC ON)
+set(TINT_BUILD_HLSL_WRITER OFF)
+set(TINT_ENABLE_IR_DUMPING OFF)
+set(TINT_BUILD_IR_BINARY OFF)
