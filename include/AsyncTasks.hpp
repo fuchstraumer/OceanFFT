79a87d5675ba30155775d08a09a5d5899f43d365
Fuchs <fuchstraumer@gmail.com>
2026-08-08 10:06:20 -0700

    De-template-ify AsyncTasks, refactor
    - Remove templates as it makes a lot of what we're trying to do loads harder (circular includes)
    - Move as much as we can to the source file
    - Clean up pointless `using` decls
    - Move AsyncSlotType somewhere else (next commit)

--- include/AsyncTasks.hpp
+++ include/AsyncTasks.hpp
@@ -1,8 +1,12 @@
 #pragma once
 #ifndef VELOX_RHI_ASYNC_TASKS_HPP
 #define VELOX_RHI_ASYNC_TASKS_HPP
-#include "Context.hpp"
+#include "utility/SlotMap.hpp"
+#include "VeloxErrors.hpp"
 #include <coroutine>
+#include <expected>
+#include <memory>
+#include <webgpu/webgpu_cpp.h>
 
 #ifndef NDEBUG
 #include <magic_enum/magic_enum.hpp>
@@ -22,6 +26,8 @@
 namespace velox
 {
 
+struct Scheduler;
+
 namespace detail
 {
 
@@ -36,19 +42,11 @@ namespace detail
         std::println(stderr, "[velox][async] {} | status: {} | message: {}", ourMsg, statusText, wgpuMessage);
 #endif
     }
+
 } // namespace detail
 
 template<typename T>
 using Result = std::expected<T, RhiError>;
-
-template<wgpu::MapMode Mode>
-using MappedPointerType = std::conditional_t<Mode == wgpu::MapMode::Read, const void*, void*>;
-
-template<wgpu::MapMode Mode>
-using MapResultType = Result<MappedPointerType<Mode>>;
-
-template<typename T>
-concept PipelineType = std::is_same_v<T, wgpu::RenderPipeline> || std::is_same_v<T, wgpu::ComputePipeline>;
 
 struct AdapterAwaitable
 {
@@ -65,6 +63,11 @@ struct AdapterAwaitable
     void await_suspend(std::coroutine_handle<> handle);
 
     Result<wgpu::Adapter> await_resume() noexcept;
+
+    constexpr explicit operator bool() const noexcept
+    {
+        return result.has_value();
+    }
 };
 
 struct DeviceAwaitable
@@ -88,107 +91,76 @@ struct DeviceAwaitable
     }
 };
 
-template<wgpu::MapMode Mode>
-struct BufferMapAwaitable
+struct MapReadAwaitable
 {
 private:
-    // checking wgpu headers, this is just 8 bytes, so a reference doesn't save us anything
     wgpu::Buffer buffer{};
     size_t size{ std::numeric_limits<size_t>::max() };
     size_t offset{ 0u };
-    MapResultType<Mode> result;
-    Context* context{ nullptr };
+    Result<const void*> result;
+    Scheduler* scheduler{ nullptr };
 
 public:
-    constexpr BufferMapAwaitable(wgpu::Buffer _buffer,
-                                 size_t _size,
-                                 size_t _offset = 0u,
-                                 Context* _context = nullptr) noexcept
-        : buffer{ _buffer },
-          size{ _size },
-          offset{ _offset },
-          context{ _context }
-    {
-    }
+    MapReadAwaitable(wgpu::Buffer _buffer,
+                     size_t _size,
+                     size_t _offset = 0u,
+                     Scheduler* _scheduler = nullptr) noexcept;
 
     constexpr bool await_ready() const noexcept
     {
         return false;
     }
 
-    void await_suspend(std::coroutine_handle<> handle)
-    {
-        // await suspend registers the callback, meaning we immediately return to the caller
-        // webgpu will call this captured callback, at which point it resumes the coroutine
+    void await_suspend(std::coroutine_handle<> handle);
 
-        // this is a deferred resume coroutine, register with context
-        SlotMapHandle slot;
-        if (context)
-        {
-            slot = context->RegisterPending(handle.address());
-        }
-
-        buffer.MapAsync(Mode,
-                        offset,
-                        size,
-                        wgpu::CallbackMode::AllowSpontaneous,
-                        [handle, slot, this](wgpu::MapAsyncStatus status, wgpu::StringView message)
-                        {
-                            if (status != wgpu::MapAsyncStatus::Success)
-                            {
-                                detail::PrintStatusMessage("MapBuffer", status, message);
-                                result = std::unexpected(RhiError::MapAsyncFailed);
-                            }
-
-                            if (!context)
-                            {
-                                handle.resume();
-                            }
-                            else
-                            {
-                                context->MarkReady(slot);
-                            }
-                        });
-    }
-
-    MapResultType<Mode> await_resume() const noexcept
-    {
-        if (!result.has_value())
-        {
-            return result;
-        }
-        else [[likely]]
-        {
-            if constexpr (Mode == wgpu::MapMode::Read)
-            {
-                return buffer.GetConstMappedRange(offset, size);
-            }
-            else if constexpr (Mode == wgpu::MapMode::Write)
-            {
-                return buffer.GetMappedRange(offset, size);
-            }
-        }
-    }
+    Result<const void*> await_resume() const noexcept;
 };
 
-template<wgpu::MapMode Mode>
-struct BufferMapSession
+struct MapWriteAwaitable
 {
+private:
+    wgpu::Buffer buffer{};
+    size_t size{ std::numeric_limits<size_t>::max() };
+    size_t offset{ 0u };
+    Result<void*> result;
+    Scheduler* scheduler{ nullptr };
+
 public:
-    BufferMapSession(const BufferMapSession&) = delete;
-    BufferMapSession& operator=(const BufferMapSession&) = delete;
-    ~BufferMapSession()
+    MapWriteAwaitable(wgpu::Buffer _buffer,
+                      size_t _size,
+                      size_t _offset = 0u,
+                      Scheduler* _scheduler = nullptr) noexcept;
+
+    constexpr bool await_ready() const noexcept
+    {
+        return false;
+    }
+
+    void await_suspend(std::coroutine_handle<> handle);
+
+    Result<void*> await_resume() const noexcept;
+};
+
+template<typename MapType>
+struct MapSession
+{
+    using PointerType = std::conditional_t<std::is_same_v<MapReadAwaitable, MapType>, const void*, void*>;
+public:
+    MapSession(const MapSession&) = delete;
+    MapSession& operator=(const MapSession&) = delete;
+    ~MapSession()
     {
         buffer.Unmap();
     }
 
-    // static Task<BufferMapSession<Mode>> CreateAsync(wgpu::Buffer buffer, size_t _size, size_t _offset)
+    // static Task<MapSession<Mode>> CreateAsync(wgpu::Buffer buffer, size_t _size, size_t _offset)
     //{
     //     MapResultType<Mode> mapResult = co_await BufferMapAwaitable<Mode>{ buffer, _size, _offset };
-    //     co_return BufferMapSession<Mode>(buffer, _size, _offset, data);
+    //     co_return MapSession<Mode>(buffer, _size, _offset, data);
     // }
 
     template<typename T>
+        requires(std::is_same_v<MapType, MapWriteAwaitable>)
     std::span<T> GetDataAs() noexcept
     {
         assert(reinterpret_cast<std::uintptr_t>(mappedPtr) % alignof(T) == 0);
@@ -200,227 +172,92 @@ public:
     }
 
     template<typename T>
+        requires(std::is_same_v<MapType, MapReadAwaitable>)
     std::span<const T> GetDataAs() const noexcept
     {
         assert(reinterpret_cast<std::uintptr_t>(mappedPtr) % alignof(T) == 0);
-        static_assert(std::is_standard_layout_v<T> && std::is_trivially_copyable_v<T>,
-                      "T must be standard layout and trivially copyable to interpret mapped data as span of T");
+        static_assert(
+            std::is_standard_layout_v<T> && std::is_trivially_copyable_v<T>,
+            "T must be standard layout and trivially copyable to interpret mapped data as span of T");
         const size_t numElements = size / sizeof(T);
         const T* typeArray = std::start_lifetime_as_array<T>(mappedPtr, numElements);
         return std::span<const T>(typeArray, typeArray + numElements);
     }
 
-    MappedPointerType<Mode> GetDataPtr() noexcept
+    void* GetDataPtr() noexcept requires(std::is_same_v<MapType, MapWriteAwaitable>)
     {
-        return data;
+        return mappedPtr;
     }
 
-    MappedPointerType<Mode> GetDataPtr() const noexcept
+    const void* GetDataPtr() const noexcept requires(std::is_same_v<MapType, MapReadAwaitable>)
     {
-        return data;
+        return mappedPtr;
     }
 
 private:
-    BufferMapSession(wgpu::Buffer _buffer,
-                     size_t _size,
-                     size_t _offset,
-                     MappedPointerType<Mode> _data) noexcept
+    MapSession(wgpu::Buffer _buffer, size_t _size, size_t _offset, PointerType _data) noexcept
         : buffer{ _buffer },
           size{ _size },
           offset{ _offset },
-          data{ _data }
+          mappedPtr{ _data }
     {
     }
     wgpu::Buffer buffer;
     size_t size;
     size_t offset;
-    MappedPointerType<Mode> data;
+    PointerType mappedPtr;
 };
 
-template<PipelineType Pipeline>
-struct PipelineAwaitable
+struct RenderPipelineAwaitable
 {
 private:
     wgpu::Device device{};
-    using DescriptorType = std::conditional_t<std::is_same_v<Pipeline, wgpu::RenderPipeline>,
-                                              wgpu::RenderPipelineDescriptor,
-                                              wgpu::ComputePipelineDescriptor>;
-    DescriptorType descriptor{};
-    Result<Pipeline> result{};
-    Context* context{ nullptr };
+    wgpu::RenderPipelineDescriptor descriptor{};
+    Result<wgpu::RenderPipeline> result{};
+    Scheduler* scheduler{ nullptr };
 
 public:
-    // try to forward the descriptor since it contains a whole boatload of members
-    constexpr PipelineAwaitable(wgpu::Device _device, DescriptorType _descriptor, Context* _context) noexcept
-        : device{ _device },
-          descriptor{ std::forward<DescriptorType>(_descriptor) },
-          context{ _context }
+    RenderPipelineAwaitable(wgpu::Device _device,
+                            wgpu::RenderPipelineDescriptor _descriptor,
+                            Scheduler* _scheduler) noexcept;
+    ~RenderPipelineAwaitable() noexcept;
+    RenderPipelineAwaitable(const RenderPipelineAwaitable&) = delete;
+    RenderPipelineAwaitable& operator=(const RenderPipelineAwaitable&) = delete;
+    
+    // todo: in the future, we could probably query a cache here and immediate return already built pipelines!
+    // alternatively: hook them up to the same coroutine slot, so they're alerted on wakeup too?
+    constexpr bool await_ready() const noexcept
     {
+        return false;
     }
 
-    ~PipelineAwaitable() noexcept
-    {
-    }
+    void await_suspend(std::coroutine_handle<> handle);
+    Result<wgpu::RenderPipeline> await_resume() noexcept;
+
+};
+
+struct ComputePipelineAwaitable
+{
+private:
+    wgpu::Device device{};
+    wgpu::ComputePipelineDescriptor descriptor{};
+    Result<wgpu::ComputePipeline> result{};
+    Scheduler* scheduler{ nullptr };
+public:
+    ComputePipelineAwaitable(wgpu::Device _device,
+                             wgpu::ComputePipelineDescriptor _descriptor,
+                             Scheduler* _scheduler) noexcept;
+    ~ComputePipelineAwaitable() noexcept;
+    ComputePipelineAwaitable(const ComputePipelineAwaitable&) = delete;
+    ComputePipelineAwaitable& operator=(const ComputePipelineAwaitable&) = delete;
 
     constexpr bool await_ready() const noexcept
     {
         return false;
     }
 
-    void await_suspend(std::coroutine_handle<> handle)
-    {
-        SlotMapHandle slot;
-        if (context)
-        {
-            slot = context->RegisterPending(handle.address());
-        }
-
-        if constexpr (std::is_same_v<Pipeline, wgpu::RenderPipeline>)
-        {
-            auto callback = [handle, slot, this](wgpu::CreatePipelineAsyncStatus status,
-                                                 wgpu::RenderPipeline pipeline,
-                                                 wgpu::StringView message)
-            {
-                if (status != wgpu::CreatePipelineAsyncStatus::Success)
-                {
-                    detail::PrintStatusMessage("CreateRenderPipelineAsync", status, message);
-                    result = std::unexpected(RhiError::PipelineCreationFailed);
-                }
-                else [[likely]]
-                {
-                    result = pipeline;
-                }
-
-                if (context)
-                {
-                    context->MarkReady(slot);
-                }
-                else
-                {
-                    handle.resume();
-                }
-            };
-
-            device.CreateRenderPipelineAsync(descriptor, wgpu::CallbackMode::AllowSpontaneous, callback);
-        }
-        else if constexpr (std::is_same_v<Pipeline, wgpu::ComputePipeline>)
-        {
-            auto callback = [handle, this](wgpu::CreatePipelineAsyncStatus status,
-                                           wgpu::ComputePipeline pipeline,
-                                           wgpu::StringView message)
-            {
-                if (status != wgpu::CreatePipelineAsyncStatus::Success)
-                {
-                    detail::PrintStatusMessage("CreateComputePiplineAsync", status, message);
-                    result = std::unexpected(RhiError::PipelineCreationFailed);
-                }
-                else [[likely]]
-                {
-                    result = pipeline;
-                }
-
-                if (context)
-                {
-                    context->MarkReady(slot);
-                }
-                else
-                {
-                    handle.resume();
-                }
-            };
-            device.CreateComputePipelineAsync(descriptor, wgpu::CallbackMode::AllowSpontaneous, callback);
-        }
-    }
-
-    Result<Pipeline> await_resume() const noexcept
-    {
-        return result;
-    }
-};
-
-// AsyncSlotType is what we store in our actual slotmap object, created when webgpu fires the callback
-// from the event processing loop. This is typed for each result type we have, and stores a result
-// and a continuation handle that we selectively resume ourselves. We need to be able to resume
-// ourselves to make sure we're in a state ready to resume and work, not just being bombarded with
-// surprise coroutine resumptions
-template<typename T>
-struct AsyncSlotType
-{
-    struct promise_type
-    {
-        T result_value;
-        // map we will publish back into
-        SlotMap<T, 512>* map;
-
-        AsyncSlotType<T> get_return_object() noexcept
-        {
-            return AsyncSlotType<T>{ std::coroutine_handle<promise_type>::from_promise(*this) };
-        }
-
-        // don't suspend initially, because we call the async function and let the awaitable
-        // that constructs be what first sends us into suspension (after enqueuing our action)
-        constexpr std::suspend_never initial_suspend() noexcept
-        {
-            // only initial step we perform: set result to nullopt, so try-get fails
-            // until we actually fill it with a value on op complete
-            result_value = std::nullopt;
-            return {};
-        }
-
-        template<typename Awaitable>
-        Awaitable&& await_transform(Awaitable&& await_inst) noexcept
-        {
-            static_assert("Didn't define appropriate await_transform for current type.");
-        }
-
-        // always suspend at the end so we can extract the result before destruction
-        constexpr std::suspend_always final_suspend() noexcept
-        {
-            return {};
-        }
-
-        template<typename U>
-        void return_value(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
-        {
-            result_value = std::forward<U>(value);
-        }
-
-        void unhandled_exception()
-        {
-#ifndef __EMSCRIPTEN__
-            std::terminate();
-#else
-            emscripten_force_exit(1);
-#endif
-        }
-    };
-
-    std::coroutine_handle<promise_type> handle;
-
-    explicit AsyncSlotType(std::coroutine_handle<promise_type> _handle) noexcept
-        : handle{ _handle }
-    {
-    }
-
-    AsyncSlotType(const AsyncSlotType&) = delete;
-    AsyncSlotType& operator=(const AsyncSlotType&) = delete;
-
-    AsyncSlotType(AsyncSlotType&& other) noexcept = default;
-    AsyncSlotType& operator=(AsyncSlotType&& other) = default;
-
-    ~AsyncSlotType() = default;
-
-    std::optional<T> TryGet()
-    {
-        if (!handle.done())
-        {
-            return std::nullopt;
-        }
-        else
-        {
-            return handle.promise().result_value;
-        }
-    }
+    void await_suspend(std::coroutine_handle<> handle);
+    Result<wgpu::ComputePipeline> await_resume() noexcept;
 };
 
 } // namespace velox
--- src/AsyncTasks.cpp
+++ src/AsyncTasks.cpp
@@ -1,57 +1,266 @@
 #include "AsyncTasks.hpp"
+#include "Scheduler.hpp"
+#include "utility/SlotMap.hpp"
 
 namespace velox
 {
 
-    void AdapterAwaitable::await_suspend(std::coroutine_handle<> handle)
-    {
-        instance.RequestAdapter(
-            &options,
-            wgpu::CallbackMode::AllowSpontaneous,
-            [this, handle](wgpu::RequestAdapterStatus status, wgpu::Adapter adapter, wgpu::StringView message)
+void AdapterAwaitable::await_suspend(std::coroutine_handle<> handle)
+{
+    instance.RequestAdapter(
+        &options,
+        wgpu::CallbackMode::AllowSpontaneous,
+        [this, handle](wgpu::RequestAdapterStatus status, wgpu::Adapter adapter, wgpu::StringView message)
+        {
+            if (status != wgpu::RequestAdapterStatus::Success)
             {
-                if (status != wgpu::RequestAdapterStatus::Success)
-                {
-                    detail::PrintStatusMessage("RequestAdapter", status, message);
-                    result = std::unexpected(velox::RhiError::AdapterRequestFailed);
-                }
-                else [[likely]]
-                {
-                    result = adapter;
-                }
-                handle.resume();
-            });
-    }
-
-    Result<wgpu::Adapter> AdapterAwaitable::await_resume()
-    {
-        return std::move(result);
-    }
-
-    void DeviceAwaitable::await_suspend(std::coroutine_handle<> handle)
-    {
-        adapter.RequestDevice(
-            &descriptor,
-            wgpu::CallbackMode::AllowSpontaneous,
-            [this, handle](wgpu::RequestDeviceStatus status, wgpu::Device device, wgpu::StringView message)
+                detail::PrintStatusMessage("RequestAdapter", status, message);
+                result = std::unexpected(velox::RhiError::AdapterRequestFailed);
+            }
+            else [[likely]]
             {
-                if (status != wgpu::RequestDeviceStatus::Success)
-                {
-                    detail::PrintStatusMessage("RequestDevice", status, message);
-                    result = std::unexpected(velox::RhiError::DeviceRequestFailed);
-                }
-                else [[likely]]
-                {
-                    result = device;
-                }
-                handle.resume();
-            });
-    }
-
-    Result<wgpu::Device> DeviceAwaitable::await_resume() noexcept
-    {
-        return std::move(result);
-    }
-
-
+                result = adapter;
+            }
+            handle.resume();
+        });
 }
+
+Result<wgpu::Adapter> AdapterAwaitable::await_resume() noexcept
+{
+    return std::move(result);
+}
+
+void DeviceAwaitable::await_suspend(std::coroutine_handle<> handle)
+{
+    adapter.RequestDevice(
+        &descriptor,
+        wgpu::CallbackMode::AllowSpontaneous,
+        [this, handle](wgpu::RequestDeviceStatus status, wgpu::Device device, wgpu::StringView message)
+        {
+            if (status != wgpu::RequestDeviceStatus::Success)
+            {
+                detail::PrintStatusMessage("RequestDevice", status, message);
+                result = std::unexpected(velox::RhiError::DeviceRequestFailed);
+            }
+            else [[likely]]
+            {
+                result = device;
+            }
+            handle.resume();
+        });
+}
+
+Result<wgpu::Device> DeviceAwaitable::await_resume() noexcept
+{
+    return std::move(result);
+}
+
+MapReadAwaitable::MapReadAwaitable(wgpu::Buffer _buffer,
+                                   size_t _size,
+                                   size_t _offset,
+                                   Scheduler* _scheduler) noexcept
+    : buffer{ _buffer },
+      size{ _size },
+      offset{ _offset },
+      scheduler{ _scheduler }
+{
+}
+
+void MapReadAwaitable::await_suspend(std::coroutine_handle<> handle)
+{
+    // await suspend registers the callback, meaning we immediately return to the caller
+    // webgpu will call this captured callback, at which point it resumes the coroutine
+
+    // this is a deferred resume coroutine, register with context
+    SlotMapHandle slot;
+    if (scheduler)
+    {
+        slot = scheduler->RegisterPending(handle.address());
+    }
+
+    buffer.MapAsync(wgpu::MapMode::Read,
+                    offset,
+                    size,
+                    wgpu::CallbackMode::AllowSpontaneous,
+                    [handle, &slot, this](wgpu::MapAsyncStatus status, wgpu::StringView message)
+                    {
+                        if (status != wgpu::MapAsyncStatus::Success)
+                        {
+                            detail::PrintStatusMessage("MapBuffer", status, message);
+                            result = std::unexpected(RhiError::MapAsyncFailed);
+                        }
+
+                        if (!scheduler)
+                        {
+                            handle.resume();
+                        }
+                        else
+                        {
+                            scheduler->MarkReady(std::move(slot));
+                        }
+                    });
+}
+
+Result<const void*> MapReadAwaitable::await_resume() const noexcept
+{
+    if (!result.has_value())
+    {
+        return result;
+    }
+    else [[likely]]
+    {
+        return buffer.GetConstMappedRange(offset, size);
+    }
+}
+
+MapWriteAwaitable::MapWriteAwaitable(wgpu::Buffer _buffer,
+                                     size_t _size,
+                                     size_t _offset,
+                                     Scheduler* _scheduler) noexcept
+    : buffer{ _buffer },
+      size{ _size },
+      offset{ _offset },
+      scheduler{ _scheduler }
+{
+}
+
+void MapWriteAwaitable::await_suspend(std::coroutine_handle<> handle)
+{
+    // await suspend registers the callback, meaning we immediately return to the caller
+    // webgpu will call this captured callback, at which point it resumes the coroutine
+
+    // this is a deferred resume coroutine, register with context
+    SlotMapHandle slot;
+    if (scheduler)
+    {
+        slot = scheduler->RegisterPending(handle.address());
+    }
+
+    buffer.MapAsync(wgpu::MapMode::Write,
+                    offset,
+                    size,
+                    wgpu::CallbackMode::AllowSpontaneous,
+                    [handle, &slot, this](wgpu::MapAsyncStatus status, wgpu::StringView message)
+                    {
+                        if (status != wgpu::MapAsyncStatus::Success)
+                        {
+                            detail::PrintStatusMessage("MapBuffer", status, message);
+                            result = std::unexpected(RhiError::MapAsyncFailed);
+                        }
+
+                        if (!scheduler)
+                        {
+                            handle.resume();
+                        }
+                        else
+                        {
+                            scheduler->MarkReady(std::move(slot));
+                        }
+                    });
+}
+
+Result<void*> MapWriteAwaitable::await_resume() const noexcept
+{
+    if (!result.has_value())
+    {
+        return result;
+    }
+    else [[likely]]
+    {
+        return buffer.GetMappedRange(offset, size);
+    }
+}
+
+RenderPipelineAwaitable::RenderPipelineAwaitable(wgpu::Device _device,
+                                                 wgpu::RenderPipelineDescriptor _descriptor,
+                                                 Scheduler* _scheduler) noexcept
+    : device{ _device },
+      descriptor{ std::forward<wgpu::RenderPipelineDescriptor>(_descriptor) },
+      scheduler{ _scheduler }
+{
+}
+
+void RenderPipelineAwaitable::await_suspend(std::coroutine_handle<> handle)
+{
+    SlotMapHandle slot;
+    if (scheduler)
+    {
+        slot = scheduler->RegisterPending(handle.address());
+    }
+
+    auto callback = [handle, &slot, this](wgpu::CreatePipelineAsyncStatus status,
+                                          wgpu::RenderPipeline pipeline,
+                                          wgpu::StringView message)
+    {
+        if (status != wgpu::CreatePipelineAsyncStatus::Success)
+        {
+            detail::PrintStatusMessage("CreateRenderPipelineAsync", status, message);
+            result = std::unexpected(RhiError::PipelineCreationFailed);
+        }
+        else [[likely]]
+        {
+            result = pipeline;
+        }
+
+        if (scheduler)
+        {
+            RhiError readyResult = scheduler->MarkReady(slot);
+            if (readyResult != RhiError::Success) [[unlikely]]
+            {
+                result = std::unexpected(readyResult);
+            }
+        }
+        else
+        {
+            handle.resume();
+        }
+    };
+
+    device.CreateRenderPipelineAsync(descriptor, wgpu::CallbackMode::AllowSpontaneous, callback);
+}
+
+Result<wgpu::RenderPipeline> RenderPipelineAwaitable::await_resume() noexcept
+{
+    return std::move(result);
+}
+
+void ComputePipelineAwaitable::await_suspend(std::coroutine_handle<> handle)
+{
+    SlotMapHandle slot;
+    if (scheduler)
+    {
+        slot = scheduler->RegisterPending(handle.address());
+    }
+
+    auto callback = [handle, this](wgpu::CreatePipelineAsyncStatus status,
+                                   wgpu::ComputePipeline pipeline,
+                                   wgpu::StringView message)
+    {
+        if (status != wgpu::CreatePipelineAsyncStatus::Success)
+        {
+            detail::PrintStatusMessage("CreateComputePiplineAsync", status, message);
+            result = std::unexpected(RhiError::PipelineCreationFailed);
+        }
+        else [[likely]]
+        {
+            result = pipeline;
+        }
+
+        if (scheduler)
+        {
+            scheduler->MarkReady(slot);
+        }
+        else
+        {
+            handle.resume();
+        }
+    };
+    device.CreateComputePipelineAsync(descriptor, wgpu::CallbackMode::AllowSpontaneous, callback);
+}
+
+Result<wgpu::ComputePipeline> ComputePipelineAwaitable::await_resume() noexcept
+{
+    return std::move(result);
+}
+
+} // namespace velox
