d5cab0ff70239e7117ad230556b3114e68ffb5c0
Fuchs <fuchstraumer@gmail.com>
2026-08-08 10:08:25 -0700

    Cleanup and fixes for VeloxErrors
    - We didn't have a success value. Don't look at me.
    - Bumped start of failure values to 10, in case we want to aid some more info/partial success values that aren't explicit errors
    - Removed WGPU_TRY
    - Removed a whole pile of unused includes
    - Put std::println under a debug define so it's stripped in release builds (might need work on how to handle this, though)

--- include/VeloxErrors.hpp
+++ include/VeloxErrors.hpp
@@ -1,85 +1,89 @@
 #pragma once
 #ifndef VELOX_ERRORS_HPP
 #define VELOX_ERRORS_HPP
-#include <string>
 #include <expected>
-#include <print>
-#include <format>
 #include <type_traits>
-#include <source_location>
 #include <webgpu/webgpu_cpp.h>
+
+#ifndef NDEBUG
+#include <print>
 #include <magic_enum/magic_enum.hpp>
+#endif
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
-// Assert-style wrappers for various WGPU functions using std::expected. A lot of this comes from the following repo,
-// but also this is probably just going to  be a common pattern across most WebGPU apps I think.... 
-// (im mostly happy to see someone else using concepts and constraints in the wild!)
+// Assert-style wrappers for various WGPU functions using std::expected. A lot of this comes from the
+// following repo, but also this is probably just going to  be a common pattern across most WebGPU apps I
+// think.... (im mostly happy to see someone else using concepts and constraints in the wild!)
 // https://github.com/dj2/Dusk/blob/main/src/common/wgpu.h
 // https://github.com/dj2/Dusk/blob/main/src/common/expected.h
 
 namespace velox
 {
 
-    enum class RhiError : uint32_t
-    {
-        // WebGPU errors
-        InstanceRequestFailed = 0,
-        AdapterRequestFailed = 1,
-        DeviceRequestFailed = 2,
-        SurfaceCreationFailed = 3,
-        SurfaceConfigurationFailed = 4,
-        SurfaceAcquireFailed = 5,
-        SurfacePresentFailed = 6,
-        // Dawn-specific errors
-        // GLFW errors
-        GLFWInitFailed = 300,
-        GLFWWindowCreationFailed = 301,
-        // Imgui errors
-        ImguiContextInitFailed = 400,
-    };
+enum class RhiError : uint64_t
+{
+    // WebGPU errors
+    Success = 0,
+    InstanceRequestFailed = 10,
+    AdapterRequestFailed = 11,
+    DeviceRequestFailed = 12,
+    SurfaceCreationFailed = 13,
+    SurfaceConfigurationFailed = 14,
+    SurfaceAcquireFailed = 15,
+    SurfacePresentFailed = 16,
+    BufferMapFailed = 17,
+    PipelineCreationFailed = 18,
+    // Start async function specific error codes (as needed)
+    AsyncCallbackCanceled = 50,
+    MapAsyncAborted = 51, // what exactly does webgpu mean by this?
+    AsyncCreatePipelineValidationFailed = 52,
+    AsyncCreatePipelineInternalError = 53,
 
-    #define WGPU_TRY(expr) \
-        do \
-        { \
-            wgpu::Status result = (expr); \
-            if (result != wgpu::Status::Success) \
-            { \
-                const std::string msg = std::format("WGPU_TRY failed: {} with status {} \n", #expr, static_cast<int>(result)); \
-                return std::unexpected(msg); \
-            } \
-        } while (false); \
+    MapAsyncFailed = 7,
+    // Dawn-specific errors
+    // GLFW errors
+    GLFWInitFailed = 300,
+    GLFWWindowCreationFailed = 301,
+    // Imgui errors
+    ImguiContextInitFailed = 400,
+};
 
-    template<typename T, typename E> requires(!std::is_void_v<T>)
-    T ValidOrExit(std::expected<T, E> result)
+template<typename T, typename E> requires(!std::is_void_v<T>)
+T ValidOrExit(std::expected<T, E> result)
+{
+    if (!result)
     {
-        if (!result)
-        {
-            std::println(stderr, "ValidOrExit failure with error {}", magic_enum::enum_name(result.error()));
-#ifdef __EMSCRIPTEN__
-            emscripten_force_exit(1);
-#else
-            std::exit(1);
+#ifndef NDEBUG
+        std::println(stderr, "ValidOrExit failure with error {}", magic_enum::enum_name(result.error()));
 #endif
-        }
-        return result.value();
+#ifdef __EMSCRIPTEN__
+        emscripten_force_exit(1);
+#else
+        std::exit(1);
+#endif
     }
+    return result.value();
+}
 
-    template<typename T, typename E> requires(std::is_void_v<T>)
-    void ValidOrExit(std::expected<T, E> result)
+template<typename T, typename E> requires(std::is_void_v<T>)
+void ValidOrExit(std::expected<T, E> result)
+{
+    if (!result)
     {
-        if (!result)
-        {
-            std::println(stderr, "ValidOrExit failure: {}", magic_enum::enum_name(result.error()));
-#ifdef __EMSCRIPTEN__
-            emscripten_force_exit(1);
-#else
-            std::exit(1);
+#ifndef NDEBUG
+        std::println(stderr, "ValidOrExit failure: {}", magic_enum::enum_name(result.error()));
 #endif
-        }
+#ifdef __EMSCRIPTEN__
+        emscripten_force_exit(1);
+#else
+        std::exit(1);
+#endif
     }
 }
 
-#endif //!VELOX_ERRORS_HPP
+} // namespace velox
+
+#endif //! VELOX_ERRORS_HPP
